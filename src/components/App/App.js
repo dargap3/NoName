272b5f1c8c7e80cf1d17690a3cab025059e8035f
dargap3 <dargap@hotmail.com>
2020-05-21 14:11:27 -0500

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from '../Routes/Routes';
+
+import './App.scss';
+
+const App = () => (
+  <div>
+    <Routes />
+  </div>
+)
+
+export default App;
---
+++ src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './home.styles.scss'
+
+const Home = () => (
+  <>
+    <section className={'container'} >
+      <h1 className={'home__gif'} >N_N_M_</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia voluptas eius nobis omnis unde aut natus, ut, maiores nulla a in facilis corporis rerum odit est non tenetur commodi. Aut?
+      </p>
+    </section>
+  </>
+)
+
+export default Home;
+
