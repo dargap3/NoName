0fb2f04cfce4db1aa2babf63eb09f5735dc4d74b
dargap3 <dargap@hotmail.com>
2020-05-23 18:12:03 -0500

    adding header feature

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Landing from '../../views/Landing/Landing';
+import Home from '../../views/Home/Home';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={Landing} />
+      <Route path='/home' component={Home} />
     </Switch>
   </Router>
 )
