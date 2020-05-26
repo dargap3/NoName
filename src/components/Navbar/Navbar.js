f96a880fc737d676ca06f7f7e976b060da130b53
dargap3 <dargap@hotmail.com>
2020-05-26 10:29:36 -0500

    creating navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Footer from '../Footer/Footer'
+
+import home from '../../assets/images/home.svg';
+import './navbar.styles.scss';
+
+const Navbar = () => (
+  <nav className={'nav'}>
+    <ul className="nav__list">
+      <li className="nav__item"><NavLink to={{pathname: '/home'}} activeClassName="nav__link" className="nav__link">quiero ser un aliado</NavLink></li>
+      <li className="nav__item"><NavLink to={{pathname: '/home'}} activeClassName="nav__link" className="nav__link">quiero conocer al equipo</NavLink></li>
+      <li className="nav__item"><NavLink to={{pathname: '/home'}} activeClassName="nav__link" className="nav__link">quiero conocer a los aliados</NavLink></li>
+      <li className="nav__item"><NavLink to={{pathname: '/home'}} activeClassName="nav__link" className="nav__link">quiero curiosear</NavLink></li>
+      <li className="nav__item"><NavLink to={{pathname: '/home'}} activeClassName="nav__link" className="nav__link"><img src={home} alt='go to home section' /></NavLink></li>
+    </ul>
+    <Footer />
+  </nav>
+)
+
+export default Navbar;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from '../../components/Header/Header'
+
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar'
 
 //import './home.styles.scss';
 
 const Home = () => (
   <>
-    <Header/>
+    <Navbar />
+    <Header />    
   </>
 )
 
