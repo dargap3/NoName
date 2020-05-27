4d250e746f99602ff23dbe0ad5fdd604b3f07deb
dargap3 <dargap@hotmail.com>
2020-05-26 22:01:01 -0500

    updating wanna be partner view

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Landing from '../../views/Landing/Landing';
 import Home from '../../views/Home/Home';
+import WannaBePartner from '../../views/WannaBePartner/WannaBePartner'
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={Landing} />
-      <Route path='/home' component={Home} />
+      <Route exact path='/home' component={Home} />
+      <Route exact path='/quiero-ser-un-aliado' component={WannaBePartner} />
     </Switch>
   </Router>
 )
---
+++ src/views/WannaBePartner/WannaBePartner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+import './WannaBePartner.styles.scss'
+
+const WannaBePartner = () => (
+  <div className={'wanna-be-partner__container'}>
+    <Header/>
+    <section className={'wanna-be-partner'} >
+        <p className={'wanna-be-partner__body'}>
+          <strong>NO CREEMOS EN SIGLAS, CREEMOS EN LAS EMPRESAS
+          </strong>        
+        </p>
+        <p className={'wanna-be-partner__body'}>
+        Si llegaste hasta aquí es porque estás cansado de escuchar buenas ideas, 
+        que nunca se ejecutan. Si te ha pasado, <strong>hablemos.</strong>
+        </p>
+        <p className={'wanna-be-partner__body'}>Nosotros entendimos que el talento 
+          con actitud es un recurso escaso y sobretodo para las empresas.</p>
+        <p className={'wanna-be-partner__body last'}>Por eso juntamos <strong>un equipo tremendo</strong>
+         que nunca te dirá qué hacer, lo hará contigo.</p>
+      <Footer />
+    </section> 
+  </div>
+);
+
+export default WannaBePartner;
