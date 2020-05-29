fc5101b47a1cd285aff809fdec1aeac92419c2cd
dargap3 <dargap@hotmail.com>
2020-05-28 20:03:29 -0500

    adding curious component

---
+++ src/views/Curious/Curious.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+
+import './curious.styles.scss';
+
+const Curious = () => (
+  <div className={'curious__container'}>
+    <Navbar />
+    <Header />    
+    <section className={'curious'} >
+        <p className={'curious__body'}>
+          <strong>NO SOMOS UN PUNTO COM HACEMOS LAS COSAS Y PUNTO
+          </strong>        
+        </p>
+        <p className={'curious__body'}>
+        Lo maravilloso de la World Wide Web es que 
+        te lleva a lugares que nunca pensaste conocer. Has llegado a un 
+        universo SIN NOMBRE en el que las empresas <br/><strong>no aceleran, escalan.</strong>
+        </p>
+        <p className={'curious__body last'}>Quizás <strong>nuestros aliados <br/></strong>
+          o los no <strong>Nonamers<br/></strong> te lo puedan explicar mejor.</p>
+    </section>
+    <Footer />
+  </div>
+);
+
+export default Curious;
