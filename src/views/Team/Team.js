9b810c0b75437011bde578ba06a5604e3a5debbb
dargap3 <dargap@hotmail.com>
2020-06-10 11:20:03 -0500

    creating modals components in different views

--- src/views/Team/Team.js
+++ src/views/Team/Team.js
@@ -2,30 +2,32 @@ import React from 'react';
 
 import './Team.styles.scss';
 
-const Team = () => (
-  <>  	
-  	<section className={'team'}>
-  		<p className={'team__body'}>
-  		  LOS NAMES DETRÁS <br />DE NO NAME      
-  		</p>
-  		<p className={'team__body'}>
-  			<strong>Los NoNamers</strong> somos una colección de 11 seres humanos diversos y complementarios. 
-  			No somos consultores, ni asesores ni banqueros de inversión. 
-  			Somos doers que <br />#<strong>HacemosNoDecimos</strong> que las cosas pasen en las empresas más tremendas de Colombia.
-  		</p>
-  	</section>
-  	<section className={'profile-team'}>
-  	  <div />
-  	  <div />	
-  	  <div />	
-  	  <div />	
-  	  <div />	
-  	  <div />	
-  	  <div />	
-  	  <div />	
-  	  <div />	
-  	</section>
-  </>
-)
+const Team = () => {
+  return (
+    <>  	
+      <section className={'team'}>
+        <p className={'team__body'}>
+          LOS NAMES DETRÁS <br />DE NO NAME      
+        </p>
+        <p className={'team__body'}>
+          <strong>Los NoNamers</strong> somos una colección de 11 seres humanos diversos y complementarios. 
+          No somos consultores, ni asesores ni banqueros de inversión. 
+          Somos doers que <br />#<strong>HacemosNoDecimos</strong> que las cosas pasen en las empresas más tremendas de Colombia.
+        </p>
+      </section>
+      <section className={'profile-team'}>
+        <div /> 
+        <div />	
+        <div />	
+        <div />	
+        <div />	
+        <div />	
+        <div />	
+        <div />	
+        <div />	
+      </section>
+    </>
+  );
+}
 
 export default Team;
