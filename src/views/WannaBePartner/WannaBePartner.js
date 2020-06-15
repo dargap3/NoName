daf5e8754623f2075ab08f10b57419e1340976f2
dargap3 <dargap@hotmail.com>
2020-06-15 05:25:47 -0500

    adding modal form

--- src/views/WannaBePartner/WannaBePartner.js
+++ src/views/WannaBePartner/WannaBePartner.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Footer from '../../components/Footer/Footer';
+import Form from '../../components/Form/Form';
+import Modal from '../../components/Modal/Modal';
 
 import './WannaBePartner.styles.scss';
 
 const WannaBePartner = () => {
   const history = useHistory();
+
+  const [modalIsOpen, setIsModalOpen] = useState(false);
+  const onCloseModal = () => setIsModalOpen(false);
 
   return (
     <>  
@@ -17,18 +22,39 @@ const WannaBePartner = () => {
           <p className={'wanna-be-partner__body'}>
             Si llegaste hasta aquí es porque estás cansado de escuchar buenas ideas,
             que nunca se ejecutan. Si te ha pasado, 
-            <span className={'animated-text animated-text--20'}>hablemos.</span>
+            <span 
+              onClick={ () => setIsModalOpen(true) }
+              className={'animated-text animated-text--20'}
+            >
+              hablemos.
+            </span>
           </p>
           <p className={'wanna-be-partner__body'}>
             Nosotros entendimos que el talento con actitud es un recurso escaso y sobretodo para las empresas.
           </p>
           <p className={'wanna-be-partner__body last'}>
             Por eso juntamos
-            <span onClick={ () => history.push("/equipo")} className={'animated-text animated-text--18'}>un equipo tremendo</span><br/> 
+            <span
+              onClick={ () => history.push("/equipo")}
+              className={'animated-text animated-text--18'}
+            >
+              un equipo tremendo
+            </span><br/> 
             que nunca te dirá qué hacer, lo hará contigo.
           </p>
       </section> 
       <Footer />
+      <Modal modalIsOpen={modalIsOpen} closeModal={onCloseModal} closeable={false}>
+        <button className={'modal__close'} onClick={onCloseModal}>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M29 2L5 27" stroke="black" strokeWidth="4"/>
+            <line x1="1.38919" y1="2.5612" x2="30.3892" y2="30.5612" stroke="black" strokeWidth="4"/>
+          </svg>
+        </button>
+        <section className={'form__container'}>
+          <Form />        
+        </section>      
+      </Modal>
     </>
   );
 }
