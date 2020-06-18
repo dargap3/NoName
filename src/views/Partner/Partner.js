ca0fdd244648ec2bdd3af6be07e8bf8314a2dbc6
dargap3 <dargap@hotmail.com>
2020-06-18 13:19:42 -0500

    fetching data from firebase

--- src/views/Partner/Partner.js
+++ src/views/Partner/Partner.js
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory} from 'react-router-dom';
 
 import Footer from '../../components/Footer/Footer';
 
 import Carousel from '@brainhubeu/react-carousel';
 
 import image1 from '../../assets/images/image-one.png';
-import firmaDani from '../../assets/images/firmas/firmaDani.png';
+
 
 import './Partner.styles.scss';
 
+const Partner = (props) => {
+  const partnerId = props.location.pathname.split('/')[2];
+  const [query, setQuery] = useState([]); 
 
-const Partner = () => {
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('https://website-noname.firebaseio.com/Equipo.json');
+        const data = await response.json();
+        const partner = data.find( ({id}) => id === partnerId )
+        setQuery(partner);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchImages();    
+  }, []);
+
+  console.log(query)
+  
+
 	const history = useHistory();
 	const [likeOne, setLikeOne] = useState(false);
 	const [likeTwo, setLikeTwo] = useState(false);
@@ -28,7 +47,7 @@ const Partner = () => {
 	return (
     <section className={'partner-container'}>
     	<div className={'partner-signature'}>
-    		<img src={firmaDani} alt={'firma Dani'} />
+    		<img src={query.imagen0} alt={'firma NoNamer'} />
     		<div className={'arrow-back'} onClick={ () => history.goBack()}>
 	        <svg width="71" height="42" viewBox="0 0 71 42" fill="none" xmlns="http://www.w3.org/2000/svg">
 	          <path d="M71 21H17" stroke="#F4F3F1" stroke-width="9"/>
@@ -37,9 +56,11 @@ const Partner = () => {
     		</div>
     	</div>
     	<div className={'partner-description'}>
-    		<h1 className={'partner-description__title'}>Daniel Upegui</h1>
-    		<p className={'partner-description__role'}>Business Builder - Marketing</p>
-				<p>@Dupegui</p>
+    		<h1 className={'partner-description__title'}>{query.nombre}</h1>
+    		<p className={'partner-description__role'}>{query.cargo}</p>
+        <a href={query.url} target='blank'>
+				  <p>{query.instagram}</p>
+        </a>
 				<div className={'carousel-container'}>
 					<Carousel
 		        autoPlay={6000}
@@ -49,19 +70,19 @@ const Partner = () => {
 		        centered={false}       
 		        >
 		        <div>
-					  	<img src={image1} alt='foto Dani' className={'image-carousel'}/>
+					  	<img src={query.imagen1} alt='foto NoNamer' className={'image-carousel'}/>
 							<svg width="24" height="20" viewBox="0 0 24 20" fill={likeOne ? 'red' : 'black'} xmlns="http://www.w3.org/2000/svg" onClick={() => setLikeOne(!likeOne)}>
 							  <path d="M1.7782 3.09827C5.67253 -1.55591 10.0861 2.58119 11.9035 5.42541C11.9035 5.42541 17.745 -1.94376 21.6393 2.71044C27.5495 9.77392 12.2929 19 12.2929 19C12.2929 19 -2.50557 12.4066 1.7782 3.09827Z" stroke={likeOne ? 'none' : 'white'}/>
 							</svg>
 		        </div>
 					  <div>
-					  	<img src={image1} alt='foto Dani' className={'image-carousel'}/>
+					  	<img src={query.imagen2} alt='foto NoNamer' className={'image-carousel'}/>
 					  	<svg width="24" height="20" viewBox="0 0 24 20" fill={likeTwo ? 'red' : 'black'} xmlns="http://www.w3.org/2000/svg" onClick={() => setLikeTwo(!likeTwo)}>
 					  	  <path d="M1.7782 3.09827C5.67253 -1.55591 10.0861 2.58119 11.9035 5.42541C11.9035 5.42541 17.745 -1.94376 21.6393 2.71044C27.5495 9.77392 12.2929 19 12.2929 19C12.2929 19 -2.50557 12.4066 1.7782 3.09827Z" stroke={likeTwo ? 'none' : 'white'}/>
 					  	</svg>
 		        </div>
 					  <div>
-					  	<img src={image1} alt='foto Dani' className={'image-carousel'}/>
+					  	<img src={query.imagen3} alt='foto NoNamer' className={'image-carousel'}/>
 							<svg width="24" height="20" viewBox="0 0 24 20" fill={likeThree ? 'red' : 'black'} xmlns="http://www.w3.org/2000/svg" onClick={() => setLikeThree(!likeThree)}>
 							  <path d="M1.7782 3.09827C5.67253 -1.55591 10.0861 2.58119 11.9035 5.42541C11.9035 5.42541 17.745 -1.94376 21.6393 2.71044C27.5495 9.77392 12.2929 19 12.2929 19C12.2929 19 -2.50557 12.4066 1.7782 3.09827Z" stroke={likeThree ? 'none' : 'white'}/>
 							</svg>
@@ -69,9 +90,7 @@ const Partner = () => {
 					</Carousel>	
 				</div>
 				<p className={'partner-description__personality'}>
-					 <span>"Dani</span> tiene la sonrisa más grande del mundo, y eso lo define. 
-					 Cuando lo vea me va a entender. En cuanto entra a un lugar, se siente su carácter, su dinamismo, su tranquilidad. 
-					 Si tiene la oportunidad de trabajar con él me va a entender.” <span>Camilo Upegui - Hermano</span>.
+					 {query.mensaje}
 				</p>
     	</div>
     	{width > 1000 && <Footer />}
--- src/views/Team/Team.js
+++ src/views/Team/Team.js
@@ -4,23 +4,28 @@ import { animateScroll as scroll} from 'react-scroll';
 
 import Footer from '../../components/Footer/Footer';
 
-import image1 from '../../assets/images/image-one.png';
-import image2 from '../../assets/images/image-two.png';
-import image3 from '../../assets/images/image-three.png';
-import image4 from '../../assets/images/image-four.png';
-import image5 from '../../assets/images/image-five.png';
-import image6 from '../../assets/images/image-six.png';
-import image7 from '../../assets/images/image-seven.png';
-import image8 from '../../assets/images/image-eight.png';
-import image9 from '../../assets/images/image-nine.png';
-import image10 from '../../assets/images/image10.png';
-import image11 from '../../assets/images/image11.png';
-
-
 import './Team.styles.scss';
 
 const Team = () => {
+  const [query, setQuery] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('https://website-noname.firebaseio.com/Equipo.json');
+        const data = await response.json();
+        
+        setQuery(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchImages();
+  }, []);
+  console.log(query)
+
   const history = useHistory();
+
   const [ width, setWidth ] = useState(window.innerWidth);
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
@@ -30,17 +35,8 @@ const Team = () => {
     }
   }, [width]);
 
-  const handleClick = () => {
-    if (document.documentElement.scrollTop > 0) {
-      scroll.scrollToTop();
-      setTimeout(() => history.push(`/equipo/${900}`), 1000);
-    } else {
-      history.push(`/equipo/${900}`); 
-    }
-  }
-
   return (
-    <div className={'team__container'}>  	
+    <div className={'team__container'}>  	 
       <section className={'team'}>
         <p className={'team__body'}>
           LOS NAMES DETRÁS <br />DE NO NAME      
@@ -52,39 +48,22 @@ const Team = () => {
         </p>
       </section>
       <section className={'profile-team'}> {/*AQUÍ IRIA UN MAP CON LAS IMAGENES DE LOS DATOS QUE SE TRAIGAN DE INSTAGRAM*/}
-        <div onClick={handleClick}>
-          <img src={image1} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image2} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image3} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image4} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image5} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image6} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image7} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image8} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image9} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image10} alt={'person one'}/>
-        </div>
-        <div onClick={handleClick}>
-          <img src={image11} alt={'person one'}/>
-        </div>
+        {(query || []).map((item, index) => {
+          const {id, imagen1} = {...item}
+          const handleClick = () => {
+            if (document.documentElement.scrollTop > 0) {
+              scroll.scrollToTop();
+              setTimeout(() => history.push(`/equipo/${id}`), 1000);
+            } else {
+              history.push(`/equipo/${id}`); 
+            }
+          }
+            return (
+              <div key={id} onClick={handleClick}>
+                <img src={imagen1} alt='imagen NoNamer'/>            
+              </div>
+            )
+          })}
       </section>
       {width > 1000 && <Footer />}
     </div>
