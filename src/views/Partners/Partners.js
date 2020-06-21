515edae4fbbb84df81e31f1882c7537a8336c6e3
dargap3 <dargap@hotmail.com>
2020-06-20 20:01:17 -0500

    minor changes

--- src/views/Partners/Partners.js
+++ src/views/Partners/Partners.js
@@ -34,7 +34,7 @@ const Partners = () => {
       <section className={'partners'}>
         <p className={'partners__body'}>
           Los llamamos aliados 
-          <strong>porque lo que tenemos con ellos no son negocios sino conspiraciones</strong> 
+          <strong style={{margin: '0px 8px'}}>porque lo que tenemos con ellos no son negocios sino conspiraciones</strong> 
           para hacer país. Todos ellos nos han permitido meternos al lodo con ellos desde el día 1, y gracias a ellos #
           <strong>HacemosNoDecimos</strong> ⚒️
         </p> 
