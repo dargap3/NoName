b1165d6bf537b144ebe62615c7b799de1f632477
dargap3 <dargap@hotmail.com>
2020-07-07 12:50:35 -0500

    deleted useless files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { CSSTransition } from 'react-transition-group';
 
 import Navbar from '../Navbar/Navbar';
 
-import logo from '../../assets/logo/LOGOX4.jpg';
+import logo from '../../assets/logo/logox4.png';
 import hamburger from '../../assets/images/hamburger.svg';
 
 
--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-import { Context } from '../../context/Context';
+import { Context } from '../../context/Context'; 
 
 import Footer from '../../components/Footer/Footer';
 import Modal from '../../components/Modal/Modal';
@@ -45,7 +45,7 @@ const Landing = () => {
           style={{
             backgroundImage: `url('${modalInicio?.["imagen de fondo"]}')`,
             backgroundSize: 'cover',
-          }} 
+          }}
         >
         <button className={'modal__close'} onClick={handleClick}>
           <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -57,7 +57,11 @@ const Landing = () => {
           <p>{`${modalInicio?.texto[1]} `}
             <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>{modalInicio?.texto[2]}</span></p>
           <p>{modalInicio?.texto[3]}</p>
-          <Link to='/home' onClick={onCloseModal} style={{ textDecoration: 'none' }}>
+          <Link 
+            to='/home' 
+            onClick={onCloseModal}
+            className={'modal__link--landing'}
+          >
             ok, vamos! 
             <svg width="42" height="24" viewBox="0 0 42 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M41.0607 13.0607C41.6464 12.4749 41.6464 11.5251 41.0607 10.9393L31.5147 1.3934C30.9289 0.807611 29.9792 0.807611 29.3934 1.3934C28.8076 1.97919 28.8076 2.92893 29.3934 3.51472L37.8787 12L29.3934 20.4853C28.8076 21.0711 28.8076 22.0208 29.3934 22.6066C29.9792 23.1924 30.9289 23.1924 31.5147 22.6066L41.0607 13.0607ZM0 13.5H40V10.5H0V13.5Z" fill="black"/>
