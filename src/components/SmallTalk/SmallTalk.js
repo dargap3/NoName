ddcf76905eaca72acc70032fb28f818d4b856dc4
dargap3 <dargap@hotmail.com>
2020-11-11 08:43:23 -0500

    chaging video player from wistia to youtube

--- src/components/SmallTalk/SmallTalk.js
+++ src/components/SmallTalk/SmallTalk.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player/lazy';
 
 import Modal from '../Modal/Modal';
-import WistiaEmbed from '../WistiaEmbed/WistiaEmbed';
-
 
 const SmallTalk = ({media}) => {
   const [modalIsOpen, setIsModalOpen] = useState(false);
   const onCloseModal = () => setIsModalOpen(false);
 
+  const {id, imagen, video} = {...media}
+
   return (
-    <div key={media.id} className={'right__image'}>
+    <div key={id} className={'right__image'}>
       <img 
-        src={media.imagen}
+        src={imagen}
         alt={''}
         onClick={ () => setIsModalOpen(true) }
         className={'carru_image'} 
       /> 
       <Modal modalIsOpen={modalIsOpen} closeModal={onCloseModal} closeable={false} className={'small-talks-modal'}>
-        <WistiaEmbed hashedId={media.video} playerColor="#000" />
+        <div style={{padding: '56.25% 0 0 0', position: 'relative'}}>
+          <div style={{height: '100%', left: '0', position: 'absolute', top: '0', width: '100%'}}>
+            <ReactPlayer 
+              url={video} 
+              height= '100%' 
+              width= '100%'
+              controls
+            />
+          </div>
+        </div>
       </Modal> 
     </div>
   )
