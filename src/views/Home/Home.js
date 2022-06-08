0ea2445618aa46b60bd27e2cf35664ecc0eb576b
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-06-07 21:24:50 -0500

    resolviendo warnings de home

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 /* import parse from 'html-react-parser'; */
 import useWidth from '../../hooks/useWidth/useWidth';
 
