7835ea8f597ba8c8ff90c443815fd502b6fc4ddb
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-06-22 18:59:07 -0500

    BCorp Logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import React from "react";
+import BCopr from "../../assets/logo/EmpresaBCertificada_Logo2021_Blanco.png";
+import linkedin from "../../assets/images/linkedin.svg";
+import instagram from "../../assets/images/instagram.svg";
+import useWidth from "../../hooks/useWidth/useWidth";
 
-import linkedin from '../../assets/images/linkedin.svg';
-import instagram from '../../assets/images/instagram.svg';
+import "./footer.styles.scss";
 
-import './footer.styles.scss';
-
-const Footer = ({inverted}) => (
-  <footer className={`
-    ${inverted ? 'footer--dark' : '' }
+const Footer = ({ inverted }) => {
+  const width = useWidth();
+  return (
+    <footer
+      className={`
+    ${inverted ? "footer--dark" : ""}
     footer
   `}
-  >
-    <address className={'footer__text'}>
-      <p>Edificio Buró 4.0 ·</p>
-      <p>Carrera 43B # 1A Sur-7 ·</p>
-      <p>Oficina 1502 · Medellín</p> 
-    </address>
-    <ul className={'social-list'}>
-      <li>
-        <a 
-          rel="noopener noreferrer"
-          target='blank' 
-          href="https://www.linkedin.com/company/noname-colombia/?viewAsMember=true"
-        >
-          <img src={linkedin} alt='logo linkedin'/>
-        </a>
-      </li>
-      <li>
-        <a 
-          rel="noopener noreferrer" 
-          target='blank' 
-          href="https://www.instagram.com/nonamecolombia/"
-        >
-          <img src={instagram} alt='logo instagram'/>
-        </a>
-      </li>
-    </ul>
-  </footer>
-)
-
+    >
+      {width >= 750 ? null : (
+        <img src={BCopr} style={{ width: "7%" }} alt="Bcorp" />
+      )}
+      
+      <address className={"footer__text"}>
+        <p>Edificio Buró 4.0 ·</p>
+        <p>Carrera 43B # 1A Sur-7 ·</p>
+        <p>Oficina 1502 · Medellín</p>
+      </address>
+      <ul className={"social-list"}>
+        <li>
+          <a
+            rel="noopener noreferrer"
+            target="blank"
+            href="https://www.linkedin.com/company/noname-colombia/?viewAsMember=true"
+          >
+            <img src={linkedin} alt="logo linkedin" />
+          </a>
+        </li>
+        <li>
+          <a
+            rel="noopener noreferrer"
+            target="blank"
+            href="https://www.instagram.com/nonamecolombia/"
+          >
+            <img src={instagram} alt="logo instagram" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 export default Footer;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import  { useParams, useLocation} from "react-router-dom";
+import React from "react";
+import { useParams, useLocation } from "react-router-dom";
+import BCorp from "../../assets/logo/EmpresaBCertificada_Logo2021_Blanco.png";
+import useWidth from "../../hooks/useWidth/useWidth";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
-import useWidth from '../../hooks/useWidth/useWidth';
-
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-
-import './Layout.styles.scss'
+import "./Layout.styles.scss";
 
 const Layout = ({ children, path, onOpenNav, onCloseNav, showNavbar }) => {
   const { pathname } = useLocation();
@@ -14,31 +13,46 @@ const Layout = ({ children, path, onOpenNav, onCloseNav, showNavbar }) => {
 
   const width = useWidth();
 
-	return (
-      <div className={'container page'}>
-        <div 
-          className={'header-main-container'}
-          style={
-            (path === '/cool-stuff' && width <= 750) ? {marginLeft: 0} : {marginLeft: ''}
-          }
-        >
-          <Header
-            path={path}
-            onCloseNav={onCloseNav}
-            onOpenNav={onOpenNav}
-            showNavbar={showNavbar}
-          />
-          <main 
-            className={`${(path === '/nonamers/:noNamerId' || path === '/cool-stuff' || path === '/home') ? 'main--nonamer' : ''} main`}
-          >
-            { children }
-          </main>
-        </div>
-        {
-          width < 1000 && (pathname === '/nonamers' || pathname === `/nonamers/${noNamerId}`)  ? null : <Footer />
+  return (
+    <div className={"container page"}>
+      <div
+        className={"header-main-container"}
+        style={
+          path === "/cool-stuff" && width <= 750
+            ? { marginLeft: 0 }
+            : { marginLeft: "" }
         }
+      >
+        <Header
+          path={path}
+          onCloseNav={onCloseNav}
+          onOpenNav={onOpenNav}
+          showNavbar={showNavbar}
+        />
+        <main
+          className={`${
+            path === "/nonamers/:noNamerId" ||
+            path === "/cool-stuff" ||
+            path === "/home"
+              ? "main--nonamer"
+              : ""
+          } main`}
+        >
+          {children}
+          {width < 750 ? null :(
+            <div className="logo-bcorp">
+              <img src={BCorp} alt="BCopor" />
+            </div>
+          )}
+        </main>
       </div>
-	);
+      {width < 1000 &&
+      (pathname === "/nonamers" ||
+        pathname === `/nonamers/${noNamerId}`) ? null : (
+        <Footer />
+      )}
+    </div>
+  );
 };
 
 export default Layout;
